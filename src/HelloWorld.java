c127396665f5d78f169086861a13564b63d5212a
Crispin Wellington <retrogradeorbit@gmail.com>
2019-11-15 19:57:50 +0800

    add link to ticket

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld {
     // putting System.loadLibrary() here forces us
     // to mark this class to initialize at runtime
     // when building with native-image
+    // https://github.com/oracle/graal/issues/1828
     //
     // static {
     //     System.loadLibrary("HelloWorld");
